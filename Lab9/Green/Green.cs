d6502f2a7deeea3215c3541856d0b90ed83a424b
kirtimmit9 <kirtimmit@outlook.com>
2026-05-10 18:16:31 +0300

    Add files via upload

---
+++ Lab9/Green/Green.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab9.Green
+{
+    public abstract class Green
+    {
+        private string _input;
+
+        public string Input
+        {
+            get { return _input; }
+        }
+
+        protected Green(string text)
+        {
+            _input = text;
+        }
+
+        public abstract void Review();
+
+        public virtual void ChangeText(string text)
+        {
+            _input = text;
+            Review();
+        }
+    }
+}
