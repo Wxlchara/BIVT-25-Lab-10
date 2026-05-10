d729899749a0d85e177d6075297e173ff6ee8b56
kirtimmit9 <kirtimmit@outlook.com>
2026-05-10 18:05:44 +0300

    Create GreenFileManager.cs

---
+++ Lab10/Green/GreenFileManager.cs
@@ -0,0 +1,29 @@
+using Lab9.Green;
+using System;
+
+namespace Lab10.Green
+{
+    public abstract class GreenFileManager : MyFileManager, ISerializer
+    {
+        protected GreenFileManager(string name) : base(name) { }
+
+        protected GreenFileManager(string name, string folder, string file, string extension = "txt")
+            : base(name, folder, file, extension) { }
+
+        public abstract void Serialize<T>(T obj) where T : Green;
+
+        public abstract T Deserialize<T>() where T : Green;
+
+        public override void EditFile(string text)
+        {
+            if (string.IsNullOrWhiteSpace(FullPath))
+                throw new InvalidOperationException("FullPath не задан.");
+        }
+
+        public override void ChangeFileExtension(string ext)
+        {
+            if (string.IsNullOrWhiteSpace(ext))
+                throw new ArgumentException("Расширение не может быть пустым.");
+        }
+    }
+}
